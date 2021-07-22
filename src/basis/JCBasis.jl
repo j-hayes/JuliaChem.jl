6060c7d37f3f18e6364198eb0c9c86bef467035d
jackson <jacksonjhayes@gmail.com>
2021-07-22 15:42:36 -0500

    refactor so that multiple basis sets can be read by JCBasis. Successful run of 02_mp2_DFRHF.json

--- src/basis/JCBasis.jl
+++ src/basis/JCBasis.jl
@@ -51,6 +51,8 @@ function run(molecule, model; output="none")
   geometry_array::Vector{Float64} = molecule["geometry"]
   symbols::Vector{String} = molecule["symbols"]
   basis::String = model["basis"]
+  build_auxillary = haskey(model, "auxillary_basis")
+  auxillary_basis::String =  build_auxillary ? model["auxillary_basis"] : ""  
   charge::Int64 = molecule["molecular_charge"]
 
   num_atoms::Int64 = length(geometry_array)/3
@@ -111,101 +113,8 @@ function run(molecule, model; output="none")
        
       basis_set_nels += atomic_number
 
-      #== read in basis set values==#
-      shells::Dict{String,Any} = read(
-        bsed["$symbol/$basis"])
-
-      #== process basis set values into shell objects ==#
-      if MPI.Comm_rank(comm) == 0 && output >= 2
-        println("Atom #$atom_idx ($symbol):") 
-        println("  Shell     Ang. Mom.     Prim.       Exp.         Coeff.")
-      end
-      
-      for shell_num::Int64 in 1:length(shells)
-        new_shell_dict::Dict{String,Any} = shells["$shell_num"]
-
-        new_shell_am::Int64 = shell_am_mapping[new_shell_dict["Shell Type"]]
-        
-        new_shell_exp = new_shell_dict["Exponents"]
-        nprim = length(new_shell_exp)
-        
-        new_shell_coeff = new_shell_dict["Coefficients"]
-        ncoeff = length(new_shell_coeff)
-
-        #== if L shell, divide up ==# 
-        if new_shell_am == -1
-          #== s component ==#
-          if MPI.Comm_rank(comm) == 0 && output >= 2
-            for iprim in 1:nprim
-              @printf("    %d        L (s)          %d     %.6f     %.6f\n", 
-                shell_num, iprim, new_shell_exp[iprim], new_shell_coeff[iprim]) 
-            end
-          end
-
-          new_shell = JCModules.Shell(shell_id, atom_idx, atomic_number, 
-            new_shell_exp, new_shell_coeff[1:nprim],
-            atom_center, 1, nprim, pos, true)
-          push!(basis_set_shells, new_shell)
-          #display(new_shell_coeff[:,1])
-          if !shells_cxx_added[atomic_number+1]
-            push!(shells_cxx[atomic_number+1], JERI.create_shell(0, 
-              new_shell_exp, new_shell_coeff[1:nprim], atom_center))
-          end
-
-          basis_set_norb += 1 
-          shell_id += 1
-          pos += new_shell.nbas
-
-          #== p component ==#
-          if MPI.Comm_rank(comm) == 0 && output >= 2
-            for iprim in 1:nprim
-              @printf("    %d        L (p)          %d      %.6f     %.6f\n", 
-                shell_num, iprim, new_shell_exp[iprim], 
-                new_shell_coeff[nprim+iprim]) 
-            end
-          end
-          
-          new_shell = JCModules.Shell(shell_id, atom_idx, atomic_number,
-            new_shell_exp, new_shell_coeff[(nprim+1):ncoeff],
-            atom_center, 2, nprim, pos, true)
-          push!(basis_set_shells,new_shell)
-          #display(new_shell_coeff[:,2])
-          if !shells_cxx_added[atomic_number+1]
-            push!(shells_cxx[atomic_number+1], JERI.create_shell(1, 
-              new_shell_exp, new_shell_coeff[(nprim+1):ncoeff], atom_center))
-          end
-
-          basis_set_norb += 3 
-          shell_id += 1
-          pos += new_shell.nbas
-        #== otherwise accept shell as is ==#
-        else 
-          if MPI.Comm_rank(comm) == 0 && output >= 2
-            for iprim in 1:nprim
-              @printf("    %d          %s            %d      %.6f     %.6f\n", 
-                shell_num, new_shell_dict["Shell Type"], iprim, 
-                new_shell_exp[iprim], new_shell_coeff[iprim]) 
-            end
-          end
-
-          new_shell = JCModules.Shell(shell_id, atom_idx, atomic_number,
-            new_shell_exp, deepcopy(new_shell_coeff),
-            atom_center, new_shell_am, nprim, pos, true)
-          push!(basis_set_shells,new_shell)
-          if !shells_cxx_added[atomic_number+1]
-            push!(shells_cxx[atomic_number+1], JERI.create_shell(new_shell_am-1, 
-              new_shell_exp, new_shell_coeff, atom_center))
-          end 
-
-          basis_set_norb += new_shell.nbas
-          shell_id += 1
-          pos += new_shell.nbas
-        end
-
-        if MPI.Comm_rank(comm) == 0 && output >= 2
-          println(" ")
-        end
-      end
+      pos, basis_set_norb, shell_id = add_shells!(bsed, basis_set_shells, shells_cxx, shells_cxx_added, symbol, basis, 
+      shell_am_mapping, atom_idx, atomic_number, pos, atom_center, basis_set_norb, shell_id, output)
       
       shells_cxx_added[atomic_number+1] = true 
       #display(shells_cxx)
@@ -267,6 +176,107 @@ function run(molecule, model; output="none")
 
   return mol, basis_set
 end
+
+function add_shells!(bsed, basis_set_shells, shells_cxx, shells_cxx_added, symbol, basis, 
+  shell_am_mapping, atom_idx, atomic_number, pos, atom_center, basis_set_norb, shell_id, output)
+  #== read in basis set values==#
+  shells::Dict{String,Any} = read(
+    bsed["$symbol/$basis"])
+    comm=MPI.COMM_WORLD
+  #== process basis set values into shell objects ==#
+  if MPI.Comm_rank(comm) == 0 && output >= 2
+    println("Atom #$atom_idx ($symbol):") 
+    println("  Shell     Ang. Mom.     Prim.       Exp.         Coeff.")
+  end
+
+  for shell_num::Int64 in 1:length(shells)
+    new_shell_dict::Dict{String,Any} = shells["$shell_num"]
+
+    new_shell_am::Int64 = shell_am_mapping[new_shell_dict["Shell Type"]]
+    
+    new_shell_exp = new_shell_dict["Exponents"]
+    nprim = length(new_shell_exp)
+    
+    new_shell_coeff = new_shell_dict["Coefficients"]
+    ncoeff = length(new_shell_coeff)
+
+    #== if L shell, divide up ==# 
+    if new_shell_am == -1
+      #== s component ==#
+      if MPI.Comm_rank(comm) == 0 && output >= 2
+        for iprim in 1:nprim
+          @printf("    %d        L (s)          %d     %.6f     %.6f\n", 
+            shell_num, iprim, new_shell_exp[iprim], new_shell_coeff[iprim]) 
+        end
+      end
+
+      new_shell = JCModules.Shell(shell_id, atom_idx, atomic_number, 
+        new_shell_exp, new_shell_coeff[1:nprim],
+        atom_center, 1, nprim, pos, true)
+      push!(basis_set_shells, new_shell)
+      #display(new_shell_coeff[:,1])
+      if !shells_cxx_added[atomic_number+1]
+        push!(shells_cxx[atomic_number+1], JERI.create_shell(0, 
+          new_shell_exp, new_shell_coeff[1:nprim], atom_center))
+      end
+
+      basis_set_norb += 1 
+      shell_id += 1
+      pos += new_shell.nbas
+
+      #== p component ==#
+      if MPI.Comm_rank(comm) == 0 && output >= 2
+        for iprim in 1:nprim
+          @printf("    %d        L (p)          %d      %.6f     %.6f\n", 
+            shell_num, iprim, new_shell_exp[iprim], 
+            new_shell_coeff[nprim+iprim]) 
+        end
+      end
+      
+      new_shell = JCModules.Shell(shell_id, atom_idx, atomic_number,
+        new_shell_exp, new_shell_coeff[(nprim+1):ncoeff],
+        atom_center, 2, nprim, pos, true)
+      push!(basis_set_shells,new_shell)
+      #display(new_shell_coeff[:,2])
+      if !shells_cxx_added[atomic_number+1]
+        push!(shells_cxx[atomic_number+1], JERI.create_shell(1, 
+          new_shell_exp, new_shell_coeff[(nprim+1):ncoeff], atom_center))
+      end
+
+      basis_set_norb += 3 
+      shell_id += 1
+      pos += new_shell.nbas
+    #== otherwise accept shell as is ==#
+    else 
+      if MPI.Comm_rank(comm) == 0 && output >= 2
+        for iprim in 1:nprim
+          @printf("    %d          %s            %d      %.6f     %.6f\n", 
+            shell_num, new_shell_dict["Shell Type"], iprim, 
+            new_shell_exp[iprim], new_shell_coeff[iprim]) 
+        end
+      end
+
+      new_shell = JCModules.Shell(shell_id, atom_idx, atomic_number,
+        new_shell_exp, deepcopy(new_shell_coeff),
+        atom_center, new_shell_am, nprim, pos, true)
+      push!(basis_set_shells,new_shell)
+      if !shells_cxx_added[atomic_number+1]
+        push!(shells_cxx[atomic_number+1], JERI.create_shell(new_shell_am-1, 
+          new_shell_exp, new_shell_coeff, atom_center))
+      end 
+
+      basis_set_norb += new_shell.nbas
+      shell_id += 1
+      pos += new_shell.nbas
+    end
+
+    if MPI.Comm_rank(comm) == 0 && output >= 2
+      println(" ")
+    end
+  end
+  return  pos, basis_set_norb, shell_id
+end
+
 export run
 
 end
